240e4a33bdc7bd6e9b42f3a83ae8038bd1d64862
Souvik Sural <98397984+Souvik34@users.noreply.github.com>
2026-08-31 20:56:12 +0530

    feat: add favicon and update stylesheet links in root route

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -201,11 +201,16 @@ export const Route =
       ],
 
       links: [
-        {
-          rel: "stylesheet",
-          href: appCss,
-        },
-      ],
+  {
+    rel: "stylesheet",
+    href: appCss,
+  },
+  {
+    rel: "icon",
+    type: "image/svg+xml",
+    href: "/favicon.svg",
+  },
+],
     }),
 
     shellComponent: RootShell,
