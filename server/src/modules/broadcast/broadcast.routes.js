3da2e8a6e33990c0018d0b7ef8f720af36ca5678
Souvik Sural <98397984+Souvik34@users.noreply.github.com>
2026-09-02 23:39:16 +0530

    feat: add authentication middleware to broadcast creation route

--- server/src/modules/broadcast/broadcast.routes.js
+++ server/src/modules/broadcast/broadcast.routes.js
@@ -1,12 +1,16 @@
 import express from "express";
+
 import {
   createBroadcast,
 } from "./broadcast.controller.js";
+
+import { protect } from "../../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 router.post(
   "/",
+  protect,
   createBroadcast
 );
 
