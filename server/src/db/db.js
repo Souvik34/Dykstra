0ad387e9eb99ed8a59c7ccad1eec60e3ea790b53
Souvik Sural <98397984+Souvik34@users.noreply.github.com>
2026-09-15 21:20:31 +0530

    db change

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -3,14 +3,11 @@ dotenv.config();
 
 import { Pool } from "pg";
 
-console.log("ENV DB_PORT:", process.env.DB_PORT);
-
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: Number(process.env.DB_PORT),
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   max: 5,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 5000,
