8d4c96259a63e7a73ec8f49ec0011c66decfe9ca
Souvik Sural <98397984+Souvik34@users.noreply.github.com>
2026-09-20 01:09:26 +0530

    fix: correct import path for publicStats routes

--- server/src/app.js
+++ server/src/app.js
@@ -61,7 +61,7 @@ app.use("/api/v1/notifications", notificationRoutes);
 import broadcastRoutes from "./modules/broadcast/broadcast.routes.js";
 app.use("/api/v1/broadcast", broadcastRoutes);
 
-import publicStatsRoutes from "./modules/publicStats/publicStats.route.js";
+import publicStatsRoutes from "./modules/publicStats/publicStats.routes.js";
 app.use("/api/public/stats", publicStatsRoutes);
 
 app.use((err, req, res, next) => {
