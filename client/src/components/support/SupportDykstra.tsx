01aef3f3ad710ef1e556afdbfc57352295386770
Souvik Sural <98397984+Souvik34@users.noreply.github.com>
2026-09-19 13:11:28 +0530

    feat: optimize SupportDykstra component by preventing duplicate script loading

--- client/src/components/support/SupportDykstra.tsx
+++ client/src/components/support/SupportDykstra.tsx
@@ -4,12 +4,18 @@ import { useEffect } from "react";
 
 function SupportDykstra() {
   useEffect(() => {
+    if (document.querySelector('script[data-name="BMC-Widget"]')) {
+      return;
+    }
+
     const script = document.createElement("script");
 
+    script.type = "text/javascript";
     script.setAttribute("data-name", "BMC-Widget");
     script.setAttribute("data-cfasync", "false");
     script.src =
       "https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js";
+
     script.setAttribute("data-id", "dykstra");
     script.setAttribute(
       "data-description",
@@ -25,17 +31,6 @@ function SupportDykstra() {
     script.setAttribute("data-y_margin", "18");
 
     document.body.appendChild(script);
-
-    return () => {
-      script.remove();
-
-      // Remove BMC's injected widget elements when leaving dashboard
-      document
-        .querySelectorAll('[id^="bmc"], [class*="bmc"]')
-        .forEach((element) => {
-          element.remove();
-        });
-    };
   }, []);
 
   return null;
